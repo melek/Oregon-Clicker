88f10ac7c5f7f57585c1f7cc0f5f029573884aa3
TheFrogLogs <104541821+TheFrogLogs@users.noreply.github.com>
2023-06-24 05:09:08 -0700

    Updated Text Processing

--- scripts/testWIP/amountTEST.gd
+++ scripts/testWIP/amountTEST.gd
@@ -4,5 +4,5 @@ const length = 2170
 
 func _physics_process(delta):
 	var progress = get_parent().get_parent().get_node("Map/progressBar").value
-	text = str(snapped(length - ((progress/100)*length), 0.001))
+	text = "%8.1-f" % (length-(progress / 100 * length))
 
