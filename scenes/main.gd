ed7c8ca101e5a262e04de58f0fedd1596ff95fb2
TheFrogLogs <104541821+TheFrogLogs@users.noreply.github.com>
2023-06-25 03:09:19 -0700

    Changed decimal places for smoother mile counter

--- scenes/main.gd
+++ scenes/main.gd
@@ -11,7 +11,7 @@ var remaining = 2169.991
 
 func _physics_process(delta):
 	
-	mainAmount.text = "%8.3-f" % (remaining)
+	mainAmount.text = "%8.2-f" % (remaining)
 	
 	progressPercent.text = "Progress " + "%.2-f" % (100-((remaining/length)*100)) + "%"
 
