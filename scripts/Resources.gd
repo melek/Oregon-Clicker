866ede5b00a64b9c4714542157332ad909eb3bfb
TheFrogLogs <104541821+TheFrogLogs@users.noreply.github.com>
2023-06-26 06:44:31 -0700

    Fixed a bug with Resources
    Resources was looking for a price button under mainTitle, reordered the code so it wouldn't happen

--- scripts/Resources.gd
+++ scripts/Resources.gd
@@ -6,10 +6,10 @@ func _physics_process(delta):
 
 func updateResources():
 	for child in get_children():
-		var value = child.get_node("value")
-		var price = child.get_node("PriceButton")
 		var valueName = child.get_name()
 		if valueName in State.game.resources:
+			var value = child.get_node("value")
+			var price = child.get_node("PriceButton")
 			var stateAmount = str(State.game.resources[valueName].amount)
 			var statePrice = State.game.resources[valueName].price
 			value.text = stateAmount
