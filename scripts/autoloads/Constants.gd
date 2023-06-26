c9c58aa9c9db0e940474b6edddd2c552740f9106
TheFrogLogs <104541821+TheFrogLogs@users.noreply.github.com>
2023-06-26 06:01:52 -0700

    Add information to Constants and State

--- scripts/autoloads/Constants.gd
+++ scripts/autoloads/Constants.gd
@@ -2,5 +2,31 @@ extends Node2D
 
 const game = {
 	"gameName": "Oregon Clicker",
-	"journeyLength": 2170
+	"journeyLength": 2170,
+	"stride": 0.000568182,
+	"statUnits": { 
+		"mainStat" : ["Miles to Oregon", "Oregons Discovered"]
+	},
+	"upgrades": {
+		"speed" : {
+			"names" : [
+				"Reinforced Harness",
+				"High-Speed Bearings",
+				"Wagon Streamlining",
+				"Carbon Fiber Wagon", 
+				"Wagon Thrust Vectoring"
+			],
+			"multiplier" : 25
+		},
+		"cash" : {
+			"names" : [
+				"Skilled Foraging", 
+				"Better Barganing", 
+				"Resourceful Trading",
+				"Investment Strategies", 
+				"Wagon Team Sponorships"
+			],
+			"multiplier" : 25
+		},
+	}
 }
--- scripts/autoloads/State.gd
+++ scripts/autoloads/State.gd
@@ -1,11 +1,82 @@
 extends Node2D
 
-var game
+var save
+
+var game = {
+		"progress" : 0,
+		"remaining" : 2170,
+		"cash" : 0,
+		"moveStats" : {
+			"actualSpeed": 0,
+			"clickSpeed" : 5,
+			"travelTime" : 0,
+			"baseSpeed" : 0
+		},
+		"store" : {
+			"wagon" : {
+				"basePrice" : 20,
+				"costMultiplier" : 1.07},
+			"crew" : {
+				"basePrice" : 5,
+				"costMultiplier" : 1.05},
+			"oxen" : {
+				"basePrice" : 7,
+				"costMultiplier" : 1.05},
+			"hunter" : {
+				"basePrice" : 10,
+				"costMultiplier" : 1.07},
+			"compass" : {
+				"basePrice" : 30,
+				"costMultiplier" : 1.17},
+		},
+		"units" : {
+			"wagon" : {
+				"amount" : 1,
+				"statMulti" : 1,
+				"consumeMulti" : .05 # Part consumption
+			},
+			"crew" : {
+				"amount" : 1,
+				"statMulti" : .50,
+				"consumeMulti" : .1  # Food consumption
+			},
+			"oxen" : {
+				"amount" : 1,
+				"statMulti" : 4,
+				"consumeMulti" : .1 # Food consumption
+			},
+			"hunter" : {
+				"amount" : 1,
+				"statMulti" : .2,
+				"consumeMulti" : 1 # Ammo consumption
+			},
+			"compass" : {
+				"amount" : 1,
+				"statMulti" : .10
+			},
+		},
+		"resources" : {
+			"food" : {
+				"amount" : 5,
+				"price" : 1
+			},
+			"ammo" : {
+				"amount" : 10,
+				"price" : 3
+			},
+			"parts" : {
+				"amount" : 5,
+				"price" : 5
+			},
+			"rafts" : {
+				"amount" : 2,
+				"price" : 8
+			}
+		}
+	}
 
 func _ready():
 	self.new_game()
 
 func new_game():
-	game = {
-		"progress": 5
-	}
+	save = game
