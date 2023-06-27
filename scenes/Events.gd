78ebfabc021e4425fd65a2b27a9ffde56d6e4e66
melek <melekcrescent@yahoo.com>
2023-06-26 23:32:23 -0700

    Add EventLog Scene

--- scenes/Events.gd
+++ scenes/Events.gd
@@ -1,4 +1,9 @@
-extends Node2D
+extends Container
 
 # Event manager code goes here o7
 
+func _ready():
+		# DEBUG: Sample log entries
+	for i in range(10):
+		await get_tree().create_timer(0.5).timeout
+		$EventLog.add_event("Test %d!" % i)
---
+++ scripts/EventLog.gd
@@ -0,0 +1,31 @@
+extends Control
+
+# The 'bullet' character indicating each new log entry
+var prefix = "◘ "
+
+# How many log entries can we hold?
+var max_log_length: int = 10
+
+# Business logic variables
+var feedbox: VBoxContainer
+var label_theme: LabelSettings
+var label_font: FontFile = load("res://assets/fonts/PrintChar21.ttf")
+
+func _ready():
+	# Assign the feedbox where we actually append our events
+	feedbox = get_node("ScrollContainer/VBoxContainer")
+	
+	# Set up our label theme template
+	label_theme = LabelSettings.new()	
+	label_theme.font = label_font
+
+# Add an event!
+func add_event(log_text: String):
+	var new_event:Label = Label.new()
+	new_event.label_settings = label_theme
+	new_event.text = prefix + log_text
+	new_event.set_autowrap_mode(TextServer.AUTOWRAP_WORD)
+	if(feedbox.get_child_count() == max_log_length): feedbox.get_child(feedbox.get_child_count() - 1).queue_free()
+	feedbox.add_child(new_event)
+	feedbox.move_child(new_event, 0)
+	$ScrollContainer.scroll_vertical = 0;
