e1cdd2905a3e119b9601117e9bba1c98e6e93b37
TheFrogLogs <104541821+TheFrogLogs@users.noreply.github.com>
2023-06-26 18:31:41 -0700

    Added some more debug tools for cash
    2 for a little cash, 3 for mega cash

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
 func _physics_process(delta):	
-	if Input.is_action_just_pressed("DEBUG_100"):
+	if Input.is_action_pressed("DEBUG_100"):
 		State.game.remaining -= 100
+	if Input.is_action_pressed("DEBUG_CASH_100"):
+		State.game.cash += 1000
+	if Input.is_action_pressed("DEBUG_CASH_1000"):
+		State.game.cash += 10000
