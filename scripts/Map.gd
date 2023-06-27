cfc0980b56074a470dec40678bcf1b10ec9b3559
melek <melekcrescent@yahoo.com>
2023-06-26 23:33:33 -0700

    Update Map.gd to correct format string

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _physics_process(delta):
 	var progress = ( State.game.progress / Constants.game.stepLength) * 100 
-	$progressPercent.text = "Progress " + "%.2-f" % progress + "%"
+	$progressPercent.text = "Progress %.2f%%" % progress
 	$progressBar.value = progress
